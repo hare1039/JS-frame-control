2c407d47c51d8cba73a505c449b4af5419a69dad
hare1039 <hare39393@gmail.com>
2020-05-20 22:16:53 -0400

    Init playground

---
+++ main.js
@@ -0,0 +1,26 @@
+
+let show_sequence = {
+    "1-small.png": 2300.5,
+    "2-small.png": 2400.5,
+    "3-small.png": 2500.5
+};
+
+function init()
+{
+    let delta = 0;
+    for (let [imgsrc, duration] of Object.entries(show_sequence)) {
+        console.log(imgsrc);
+        let new_img = $("<img>", {id: imgsrc, src: imgsrc});
+        new_img.hide();
+        $("#display").prepend(new_img);
+
+        setTimeout(function() {
+            $("img").hide();
+            new_img.show();
+            console.log(performance.now());
+        }, delta);
+        delta += duration;
+    }
+}
+
+$(document).ready(init);
